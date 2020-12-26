203d3412ad9d3d6fa3c125e2c0cc55db0071b407
clemado1 <clemado1@gmail.com>
2020-12-27 01:48:53 +0900

    spring lec 16 di

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -9,7 +9,6 @@ public class MemberController {
 
     private final MemberService memberService;
 
-    // 컴포넌트 스캔. MemberController 생성 시 Spring Bean에 등록된 MemberService를 연결
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
