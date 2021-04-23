123931ff8b641d6c5e77d5a896b62485ce6a652c
clemado1 <clemado1@gmail.com>
2021-04-23 23:57:45 +0900

    core 중복 등록과 충돌

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -9,5 +12,14 @@ import org.springframework.context.annotation.FilterType;
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
+
+    // 수동으로 등록 시 자동으로 등록하는 빈을 Overriding 한다.
+    // Overriding bean definition for bean 'memoryMemberRepository' with a different definition
+    // 대신 Spring Boot 로 실행할 경우 에러가 발생한다.
+    // spring.main.allow-bean-definition-overriding=false 기본값
+//    @Bean(name = "memoryMemberRepository")
+//    MemberRepository memberRepository() {
+//        return new MemoryMemberRepository();
+//    }
 
 }
