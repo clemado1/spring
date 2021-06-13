295237d8b630812ba3edb8cac54aaebe16ef87f8
clemado1 <clemado1@gmail.com>
2021-06-13 23:21:50 +0900

    Interface Bean 초기화

--- core/src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
+++ core/src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -5,10 +5,10 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-public class BeanLifeCycleTest {
+class BeanLifeCycleTest {
 
     @Test
-    public void lifeCycleTest() {
+    void lifeCycleTest() {
         AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
         NetworkClient client = ac.getBean(NetworkClient.class);
         ac.close();
