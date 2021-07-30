5c363d8bf27e8e0c8246be8f32d27cb2ee1c6b31
clemado1 <clemado1@gmail.com>
2021-07-30 23:42:09 +0900

    The Java Test 프로젝트 추가

---
+++ infllearn-the-java-test/src/main/java/me/clemado1/infllearnthejavatest/InfllearnTheJavaTestApplication.java
@@ -0,0 +1,13 @@
+package me.clemado1.infllearnthejavatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfllearnTheJavaTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InfllearnTheJavaTestApplication.class, args);
+    }
+
+}
---
+++ infllearn-the-java-test/src/test/java/me/clemado1/infllearnthejavatest/InfllearnTheJavaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package me.clemado1.infllearnthejavatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfllearnTheJavaTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
