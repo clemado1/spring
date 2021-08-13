e2d5120bc5bc8dcf7f30fb69a174b4e1f0c9c479
clemado1 <clemado1@gmail.com>
2021-08-13 23:48:05 +0900

    JUnit 5 테스트 반복하기

--- infllearn-the-java-test/src/main/java/me/clemado1/infllearnthejavatest/Study.java
+++ infllearn-the-java-test/src/main/java/me/clemado1/infllearnthejavatest/Study.java
@@ -5,11 +5,18 @@ public class Study {
 
     private int limit;
 
+    private String name;
+
     public Study(int limit) {
         if (limit < 0) {
             throw new IllegalArgumentException("스터디 최대 참석 가능 인원은 0보다 커야한다.");
         }
         this.limit = limit;
+    }
+
+    public Study(int limit, String name) {
+        this.limit = limit;
+        this.name = name;
     }
 
     public StudyStatus getStudyStatus() {
@@ -18,5 +25,18 @@ public class Study {
 
     public int getLimit() {
         return limit;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Study{" +
+                "studyStatus=" + studyStatus +
+                ", limit=" + limit +
+                ", name='" + name + '\'' +
+                '}';
     }
 }
--- infllearn-the-java-test/src/test/java/me/clemado1/infllearnthejavatest/StudyTest.java
+++ infllearn-the-java-test/src/test/java/me/clemado1/infllearnthejavatest/StudyTest.java
@@ -2,6 +2,16 @@ package me.clemado1.infllearnthejavatest;
 
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.condition.*;
+import org.junit.jupiter.api.extension.ParameterContext;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.aggregator.AggregateWith;
+import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
+import org.junit.jupiter.params.aggregator.ArgumentsAggregationException;
+import org.junit.jupiter.params.aggregator.ArgumentsAggregator;
+import org.junit.jupiter.params.converter.ArgumentConversionException;
+import org.junit.jupiter.params.converter.ConvertWith;
+import org.junit.jupiter.params.converter.SimpleArgumentConverter;
+import org.junit.jupiter.params.provider.*;
 
 import java.time.Duration;
 import java.util.Optional;
@@ -50,6 +60,49 @@ class StudyTest {
             System.out.println("studyOp is present! - " + studyOp.get());
         } else {
             System.out.println("studyOp is empty! - " + studyOp);
+        }
+    }
+
+    @DisplayName("스터디 만들기")
+    @RepeatedTest(value = 10, name = "{displayName}, {currentRepetition}/{totalRepetitions}")
+    void create_study(RepetitionInfo repetitionInfo) {
+        System.out.println("test " + repetitionInfo.getCurrentRepetition() + "/" + repetitionInfo.getTotalRepetitions());
+    }
+
+    @DisplayName("스터디 만들기")
+    @ParameterizedTest(name = "{index} {displayName} message={0}")
+    @ValueSource(strings = {"1", "2", "3", "4"})
+    @NullAndEmptySource
+    void parameterizedTest(String message) {
+        System.out.println(message);
+    }
+
+    @DisplayName("스터디 만들기")
+    @ParameterizedTest(name = "{index} {displayName} message={0}")
+    @ValueSource(ints = {1, 2, 3, 4})
+    void parameterizedTest(@ConvertWith(StudyConverter.class) Study message) {
+        System.out.println(message.getLimit());
+    }
+
+    @DisplayName("스터디 만들기")
+    @ParameterizedTest(name = "{index} {displayName} message={0}")
+    @CsvSource({"10, 'java study", "20, spring study"})
+    void csvSourceTest(@AggregateWith(StudyAggregator.class) Study study) {
+        System.out.println(study);
+    }
+
+    static class StudyConverter extends SimpleArgumentConverter {
+        @Override
+        protected Object convert(Object o, Class<?> aClass) throws ArgumentConversionException {
+            assertEquals(Study.class, aClass, "Can only convert to Study");
+            return new Study(Integer.parseInt(o.toString()));
+        }
+    }
+
+    static class StudyAggregator implements ArgumentsAggregator {
+        @Override
+        public Object aggregateArguments(ArgumentsAccessor argumentsAccessor, ParameterContext parameterContext) throws ArgumentsAggregationException {
+            return new Study(argumentsAccessor.getInteger(0), argumentsAccessor.getString(1));
         }
     }
 
