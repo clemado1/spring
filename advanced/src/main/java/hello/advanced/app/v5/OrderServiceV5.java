8a0ea19ea016a69fd43c79d1c2201e778ce8ce11
clemado1 <clemado1@gmail.com>
2022-02-17 22:46:51 +0900

    template callback pattern - 적용

---
+++ advanced/src/main/java/hello/advanced/app/v5/OrderServiceV5.java
@@ -0,0 +1,29 @@
+package hello.advanced.app.v5;
+
+import hello.advanced.app.v4.OrderRepositoryV4;
+import hello.advanced.trace.callback.TraceCallback;
+import hello.advanced.trace.callback.TraceTemplate;
+import hello.advanced.trace.logtrace.LogTrace;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderServiceV5 {
+
+    private final OrderRepositoryV4 orderRepository;
+    private final TraceTemplate template;
+
+    public OrderServiceV5(OrderRepositoryV4 orderRepository, LogTrace trace) {
+        this.orderRepository = orderRepository;
+        this.template = new TraceTemplate(trace);
+    }
+
+    public void orderItem(String itemId) {
+
+        template.execute("OrderService.orderItem()", (TraceCallback<Void>) () -> {
+            orderRepository.save(itemId);
+            return null;
+        });
+
+    }
+
+}
