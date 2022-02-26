f07bec08761ad7f03967b91031a73df8cd38c349
clemado1 <clemado1@gmail.com>
2022-02-26 23:56:13 +0900

    proxy pattern - 예제1

---
+++ proxy/src/test/java/hello/proxy/pureproxy/code/ProxyPatternTest.java
@@ -0,0 +1,15 @@
+package hello.proxy.pureproxy.code;
+
+import org.junit.jupiter.api.Test;
+
+class ProxyPatternTest {
+
+    @Test
+    void noProxyTest() {
+        RealSubject realSubject = new RealSubject();
+        ProxyPatternClient client = new ProxyPatternClient(realSubject);
+        client.execute();
+        client.execute();
+        client.execute();
+    }
+}
