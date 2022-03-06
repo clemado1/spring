298a4fdb7275ba8542d592e41f1af1dfc2ff1c74
clemado1 <clemado1@gmail.com>
2022-03-06 23:38:08 +0900

    decorator pattern - 예제3

--- proxy/src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
+++ proxy/src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
@@ -1,9 +1,6 @@
 package hello.proxy.pureproxy.decorator;
 
-import hello.proxy.pureproxy.decorator.code.Component;
-import hello.proxy.pureproxy.decorator.code.DecoratorPatternClient;
-import hello.proxy.pureproxy.decorator.code.MessageDecorator;
-import hello.proxy.pureproxy.decorator.code.RealComponent;
+import hello.proxy.pureproxy.decorator.code.*;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
@@ -28,9 +25,11 @@ class DecoratorPatternTest {
     }
 
     @Test
-    void decorator() {
-        RealComponent realComponent = new RealComponent();
-        DecoratorPatternClient client = new DecoratorPatternClient(realComponent);
+    void decorator2() {
+        Component realComponent = new RealComponent();
+        Component messageDecorator = new MessageDecorator(realComponent);
+        Component timeDecorator = new TimeDecorator(messageDecorator);
+        DecoratorPatternClient client = new DecoratorPatternClient(timeDecorator);
 
         client.execute();
     }
