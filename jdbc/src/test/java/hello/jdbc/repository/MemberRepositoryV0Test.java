38229fb1dbcd675668801fa8a1a12e8850423aaa
clemado1 <clemado1@gmail.com>
2022-08-26 00:01:37 +0900

    jdbc - 수정, 삭제

--- jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -2,11 +2,14 @@ package hello.jdbc.repository;
 
 import hello.jdbc.domain.Member;
 import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
+import java.util.NoSuchElementException;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @Slf4j
 class MemberRepositoryV0Test {
@@ -16,14 +19,23 @@ class MemberRepositoryV0Test {
     @Test
     void crud() throws SQLException {
         // save
-        Member member = new Member("memberV2", 10000);
+        Member member = new Member("memberV4", 10000);
         repository.save(member);
 
         // findById
-        Member findMember = repository.findById("memberV2");
+        Member findMember = repository.findById("memberV4");
         log.info("findMember={}", findMember);
         log.info("member == findMember = {}", member == findMember);
         log.info("member equals findMember = {}", member.equals(findMember)); // lombok @Data 사용 시 객체의 모든 필드를 사용하도록 `equals()` 오버라이딩
         assertThat(findMember).isEqualTo(member);
+
+        // update: money: 10000 -> 20000
+        repository.update(member.getMemberId(), 20000);
+        Member updateMember = repository.findById(member.getMemberId());
+        assertThat(updateMember.getMoney()).isEqualTo(20000);
+
+        // delete
+        repository.delete(member.getMemberId());
+        assertThatThrownBy(() -> repository.findById(member.getMemberId())).isInstanceOf(NoSuchElementException.class);
     }
 }
