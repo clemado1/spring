04444189f0fe30b195ccccae5c9b30afd69b2236
clemado1 <clemado1@gmail.com>
2022-08-25 23:35:38 +0900

    jdbc - 조회

--- jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -1,19 +1,29 @@
 package hello.jdbc.repository;
 
 import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
+@Slf4j
 class MemberRepositoryV0Test {
 
     MemberRepositoryV0 repository = new MemberRepositoryV0();
 
     @Test
     void crud() throws SQLException {
-        Member member = new Member("memberV0", 10000);
+        // save
+        Member member = new Member("memberV2", 10000);
         repository.save(member);
+
+        // findById
+        Member findMember = repository.findById("memberV2");
+        log.info("findMember={}", findMember);
+        log.info("member == findMember = {}", member == findMember);
+        log.info("member equals findMember = {}", member.equals(findMember)); // lombok @Data 사용 시 객체의 모든 필드를 사용하도록 `equals()` 오버라이딩
+        assertThat(findMember).isEqualTo(member);
     }
 }
